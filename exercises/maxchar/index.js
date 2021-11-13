cd28e6ce3fb7812d48a0e389581e7efd7098f523
inu <inu.pragantha@gmail.com>
2021-11-13 08:00:49 +0700

    max char

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,20 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+    let mapChar ={}
+    let currentChar=''
+    
+    for(let x of str){
+        mapChar[x] = mapChar[x] + 1 || 1
+        let currentVal = mapChar[currentChar] || 0
+
+        if(mapChar[x] > currentVal){
+            currentChar = x
+        }
+    }
+
+    return currentChar
+}
 
 module.exports = maxChar;
